d536c79a674cb4351f50f63dc788a62739c98cbc
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2025-02-07 19:30:26 -0600

    commit__1
    Initial deployment of Hugo Perez painting website

---
+++ assets/main.js
@@ -0,0 +1,65 @@
+
+function callNumber() {
+    var phoneNumber = "9366524468";
+    window.location.href = "tel:" + phoneNumber;
+}
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' }); 
+    }
+}
+
+// GALLERY SWIPER JS CONTROLS
+var swiper = new Swiper('.gallery-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    speed: 800,
+    loop: true,
+  
+     autoplay: {
+         delay: 2000,
+         disableOnInteraction: true,
+       },
+  
+      navigation: {
+        nextEl: '.gallery-next',
+        prevEl: '.gallery-prev',
+    },
+    
+       breakpoints: {
+
+        744:{
+            slidesPerView: 3,
+            centeredSlides: true,
+        },
+     },
+  });
+
+
+// SCROLL REVEAL JS
+const sr = ScrollReveal({
+    distance: '80px',
+    duration: 2200,
+  })
+  
+  sr.reveal(`.section-title, .home-title`,{
+    origin: 'top',
+  })
+  
+  sr.reveal(`.section-subtitle, .home-subtitle, .p-text, .home-img, .button p`, {
+    origin: 'top',
+    delay: 400,
+    interval: 300,
+  })
+
+  sr.reveal(`.section-accent` , {
+    origin: 'left',
+    delay: 300,
+  })
+
+  sr.reveal(`.service-item-info` , {
+    origin: 'bottom',
+    interval: 200,
+  })
